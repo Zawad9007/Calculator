46e2a592e1227e2d0b178c9b3be41d43d176bfe5
Zawad9007 <112330821+Zawad9007@users.noreply.github.com>
2022-10-13 15:08:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let str = ""
+let btns = document.querySelectorAll('.btn')
+Array.from(btns).forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            str = eval(str)
+            document.querySelector('input').value = str
+        }
+        else if (e.target.innerHTML == 'C') {
+            str = ""
+            document.querySelector('input').value = str
+        }
+        else if (e.target.innerHTML == 'AC') {
+            str = "0"
+            document.querySelector('input').value = str
+        }
+
+
+      
+ 
+        else {
+            console.log("Clicking")
+            str = str + e.target.innerHTML
+            document.querySelector('input').value = str
+        }
+    })
+})
